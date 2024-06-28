955d34b1ce4ca72f9eb17d911ed12418915425da
sachintharx <139696727+sachintharx@users.noreply.github.com>
2024-06-28 23:04:24 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -36,21 +36,21 @@ pipeline {
             }
         }
 
-        stage('Free Port') {
-            steps {
-                script {
-                    // Check if the port is in use and free it if necessary
-                    def portCheck = bat(script: "netstat -ano | findstr \":${PORT}\"", returnStatus: true)
-                    if (portCheck == 0) {
-                        def pid = bat(script: "for /f \"tokens=5\" %%a in ('netstat -ano ^| findstr \":${PORT}\"') do @echo %%a", returnStdout: true).trim()
-                        echo "Killing process with PID ${pid} on port ${PORT}"
-                        bat "taskkill /PID ${pid} /F"
-                    } else {
-                        echo "Port ${PORT} is not in use."
-                    }
-                }
-            }
-        }
+        // stage('Free Port') {
+        //     steps {
+        //         script {
+        //             // Check if the port is in use and free it if necessary
+        //             def portCheck = bat(script: "netstat -ano | findstr \":${PORT}\"", returnStatus: true)
+        //             if (portCheck == 0) {
+        //                 def pid = bat(script: "for /f \"tokens=5\" %%a in ('netstat -ano ^| findstr \":${PORT}\"') do @echo %%a", returnStdout: true).trim()
+        //                 echo "Killing process with PID ${pid} on port ${PORT}"
+        //                 bat "taskkill /PID ${pid} /F"
+        //             } else {
+        //                 echo "Port ${PORT} is not in use."
+        //             }
+        //         }
+        //     }
+        // }
 
         stage('Deploy Application') {
             steps {
