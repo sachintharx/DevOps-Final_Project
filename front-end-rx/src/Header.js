8e48bd823c382ed0ad3dca6a3dfe95a8976dd110
sachintharx <139696727+sachintharx@users.noreply.github.com>
2024-06-28 22:56:57 +0530

    Update Header.js

--- front-end-rx/src/Header.js
+++ front-end-rx/src/Header.js
@@ -39,7 +39,7 @@ function Header() {
           className={styles.home}
           activeClassName={styles.active} // Use your style class for active links
         >
-          Home
+          Homeeeeee
         </NavLink>
         <NavLink
           to="/service"
